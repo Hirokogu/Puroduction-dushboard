547df9b7d156ba9e2c7b2ed253b90ab7cfc62e94
HiroKogu <86550300+HiroKogu@users.noreply.github.com.>
2024-10-04 19:49:23 +0900

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,8 +20,9 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot():void
+    public function boot(UrlGenerator $url):void
     {
+        $url->forceScheme('https');
   
     }
 }
